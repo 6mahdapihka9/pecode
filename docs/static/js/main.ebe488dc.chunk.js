1461e4d3f086fad5b08b5049308647b1a60aa393
Kulesh Aleksandr <53787083+6mahdapihka9@users.noreply.github.com>
2021-11-08 18:11:18 +0200

    initial build

---
+++ docs/static/js/main.ebe488dc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppecode=this.webpackJsonppecode||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),o=t.n(c),r=t(3),s=t.n(r),i=(t(8),t(9),t(0));var p=function(){return Object(i.jsx)("div",{className:"App",children:"App"})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(p,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ebe488dc.chunk.js.map
